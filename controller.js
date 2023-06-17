4639b0e349238c5165f67352bd8b6ed03cbf6ee0
LuisGa24 <69638645+LuisGa24@users.noreply.github.com>
2023-06-17 12:31:32 -0600

    MM1 finito

--- controller.js
+++ controller.js
@@ -55,12 +55,11 @@ function mmm(event) {
     if (document.getElementById("lambdaField").value.length == 0 || document.getElementById("miuField").value.length == 0 || document.getElementById("mField").value.length == 0) {
         alert("Hay espacios vacios. Rellene todos antes de continuar");
     } else {
-        for (let n = 0; n < m - 1; n++) {
-            var resultado = ((1 / fact(n)) * Math.pow((lambda / miu), n));
+        for (let n = 1; n <= m - 1; n++) {
+            var resultado = ((1 / fact(n)) + Math.pow((lambda / miu), n));
             sumatoria += resultado;
         }
-
-        var P0 = (1 / (sumatoria + ((1 / fact(m)) + (Math.pow((lambda / miu), m) * ((m * miu) / ((m * miu) - lambda)))))).toFixed(2);
+        var P0 = (1 / (sumatoria + ((1 / fact(m)) * (Math.pow((lambda / miu), m) * ((m * miu) / ((m * miu) - lambda)))))).toFixed(2);
         var L = (((((lambda * miu) * Math.pow(lambda / miu, m)) / (fact(m - 1) * Math.pow((m * miu) - lambda, 2)))) * P0 + (lambda / miu)).toFixed(2);
         var W = (L / lambda).toFixed(2);
         var Lq = (L - (lambda / miu)).toFixed(2);
@@ -98,4 +97,44 @@ function md1(event){
 
     document.getElementById("cardResult").style.visibility = 'visible';
         document.getElementById("cardResultText").innerHTML = "El tiempo estimado en cola es de " + 60 * Wq.toFixed(2) + " minutos, mientras que el tiempo de servicio (en el que se atiende) es de " + (W * 60).toFixed(2) + " minutos.";
+}
+
+function mm1inf(event) {
+
+    event.preventDefault()
+
+    var lambda = parseFloat(document.getElementById("lambdaField").value);
+    var miu = parseFloat(document.getElementById("miuField").value);
+    var N = parseFloat(document.getElementById("NField").value);
+
+    if (document.getElementById("lambdaField").value.length == 0 || document.getElementById("miuField").value.length == 0 || document.getElementById("NField").value.length == 0) {
+        alert("Hay espacios vacios. Rellene todos antes de continuar..");
+    } else {
+
+        var sumatoria = 0;
+        for(let n = 0; n <= N; n++){
+            sumatoria += (fact(N)/fact(N-n))*Math.pow((lambda/miu),n);
+        }
+
+        var P0Field = (1/sumatoria);//--
+        var LqField = (N- ((lambda + miu)/lambda)*(1-P0Field));//--
+        var LValue  = (LqField + (1-P0Field));//--
+        var WqField = (LqField/((N-LValue)*lambda));//--
+        var WField  = (WqField + (1/miu));//--
+        //var pField = (lambda / miu).toFixed(2) * 100;
+        //var probField = Math.pow((lambda / miu), (N + 1)).toFixed(2) * 100;
+
+
+        document.getElementById("LField").innerHTML = LValue.toFixed(2);
+        document.getElementById("WField").innerHTML = WField.toFixed(2);
+        document.getElementById("LqField").innerHTML = LqField.toFixed(2);
+        document.getElementById("WqField").innerHTML = WqField.toFixed(2);
+        //document.getElementById("pField").innerHTML = pField + "%";
+        document.getElementById("P0Field").innerHTML = (P0Field * 100).toFixed(2) + "%";
+        //document.getElementById("ProbField").innerHTML = probField + "%";
+
+        document.getElementById("cardResult").style.visibility = 'visible';
+        document.getElementById("cardResultText").innerHTML = "El tiempo estimado en cola es de " + 60 * WqField + " minutos, mientras que el tiempo de servicio (en el que se atiende) es de " + WField * 60 + " minutos.";
+    }
+
 }
